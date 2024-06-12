86abdc3665f4277cc16f67b0d8582a551796fdc9
askpng <81590263+askpng@users.noreply.github.com>
2024-06-13 02:21:15 +0700

    ex: Test cachyOS kernel override & scripts

---
+++ config/scripts/kernel-override.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+### EXAMPLE STARTS HERE ###
+
+# Your code goes here.
+echo 'Enable SElinux policy'
+setsebool -P domain_kernel_load_modules on
+
+echo 'CachyOS kernel override'
+rpm-ostree cliwrap install-to-root / && \
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-lto  
+
+echo 'Exclude official kernel from updates'
+sed -i '/^\[updates\]/a\exclude\=kernel\*' /etc/yum.repos.d/fedora-updates.repo
+
+echo 'Install uksmd'
+rpm-ostree install libcap-ng-devel procps-ng-devel uksmd
+systemctl enable uksmd.service
+
+### source: https://github.com/deus0ne/beret/blob/main/config/scripts/cachyos-kernel-override.sh ###

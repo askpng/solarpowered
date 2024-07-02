e18ab49587e3798ad63d1f27436fb1aec5f8a01f
askpng <81590263+askpng@users.noreply.github.com>
2024-07-02 12:45:26 +0700

    Add fsync kernel scripts

---
+++ config/scripts/fsync-kernel.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+### EXAMPLE STARTS HERE ###
+
+# Your code goes here.
+echo 'Replace default kernel with fsync kernel'
+rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-fsync' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+
+echo 'Exclude official kernel from updates'
+sed -i '/^\[updates\]/a\exclude\=kernel\*' /etc/yum.repos.d/fedora-updates.repo

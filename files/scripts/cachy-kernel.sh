00e603db80e51f119a6c6b6c3bfed54ce5e6eb0e
askpng <81590263+askpng@users.noreply.github.com>
2024-07-30 11:51:25 +0700

    Reorganize cachy scripts

--- files/scripts/cachy-kernel.sh
+++ files/scripts/cachy-kernel.sh
@@ -10,7 +10,4 @@ echo 'Enable SElinux policy'
 setsebool -P domain_kernel_load_modules on
 
 echo 'CachyOS kernel override'
-rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-
-echo 'Install uksmd'
-rpm-ostree install libcap-ng-devel procps-ng-devel uksmd
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos

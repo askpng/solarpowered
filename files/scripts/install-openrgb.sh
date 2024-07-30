353e98d06108ec469781c0c9274a6bc440ab0a8f
askpng <81590263+askpng@users.noreply.github.com>
2024-07-30 22:47:10 +0700

    Update install-openrgb.sh

--- files/scripts/install-openrgb.sh
+++ files/scripts/install-openrgb.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-set -eu
-set -o pipefail
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
 
 # Download udev rules file
 wget https://openrgb.org/releases/release_0.9/60-openrgb.rules

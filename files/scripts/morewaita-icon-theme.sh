750eca0464fae4e140d22fbef038c1c430811ffb
askpng <81590263+askpng@users.noreply.github.com>
2024-10-18 22:04:59 +0700

    Add morewaita icon pack script

---
+++ files/scripts/morewaita-icon-theme.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+echo 'Preparing directory for cloning...'
+
+mkdir -p /tmp/clone/MoreWaita
+cd /tmp/clone/WoreWaita
+echo 'Directory created.'
+
+git clone https://github.com/somepaulo/MoreWaita.git
+echo 'Repo cloned. Running install script...'
+
+MoreWaita/install.sh
+echo 'Install script finished. Removing cloned repo...'
+
+rm -r MoreWaita
+echo 'Cloned repo deleted.'

e6344a24271ecb5b01095d8be1d7610f1e6b0b82
askpng <81590263+askpng@users.noreply.github.com>
2025-02-16 23:45:59 +0700

    refactor: :recycle: refactor scripts for negativo-multimedia-repo and morewaita
    monitor if morewaita script works as intended

--- files/scripts/base/negativo17-multimedia-repo.sh
+++ files/scripts/base/negativo17-multimedia-repo.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Install negativo17-multimedia-repo
-echo 'Installing negativo17-multimedia-repo'
-curl -fLs --create-dirs https://negativo17.org/repos/fedora-multimedia.repo -o /etc/yum.repos.d/negativo17-fedora-multimedia.repo
-sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
-
 # Replace multimedia packages with ones from negativo17-multimedia-repo
 echo 'Replacing multimedia packages to packages from negativo17-multimedia-repo'
 rpm-ostree override replace --experimental --from repo='fedora-multimedia' \
@@ -21,9 +16,5 @@ rpm-ostree override replace --experimental --from repo='fedora-multimedia' \
     mesa-libxatracker \
     mesa-va-drivers \
     mesa-vulkan-drivers
-
-# Remove negativo17-multimedia-repo
-# echo 'Removing negativo17-multimedia-repo'
-# rm -f /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
 rpm-ostree override replace --from repo='fedora' --experimental --remove=OpenCL-ICD-Loader ocl-icd || true
--- files/scripts/shared/morewaita-icon-theme.sh
+++ files/scripts/shared/morewaita-icon-theme.sh
@@ -2,17 +2,48 @@
 
 set -oue pipefail
 
-echo 'Preparing directory for cloning...'
+GITHUB_URL="https://github.com/somepaulo/MoreWaita"
+if [ -z "$GITHUB_URL" ]; then
+  echo "Error: GITHUB_URL is not set."
+  exit 1
+fi
 
-mkdir -p /tmp/clone/MoreWaita/
-cd /tmp/clone/MoreWaita/
-echo 'Directory created.'
+REPO_NAME=$(basename "$GITHUB_URL" .git)
+CLONE_DIR="/tmp/clone/$REPO_NAME"
 
-git clone https://github.com/somepaulo/MoreWaita.git
-echo 'Repo cloned. Running install script...'
+echo "Preparing directory for cloning..."
+mkdir -p "$CLONE_DIR"
+cd "$CLONE_DIR"
+echo "Directory created."
 
-MoreWaita/install.sh
-echo 'Install script finished. Removing cloned repo...'
+git clone "$GITHUB_URL"
 
-rm -r MoreWaita
-echo 'Cloned repo deleted.'
+echo "Repo cloned. Copying files..."
+
+if [ -f "./$REPO_NAME/install.sh" ]; then
+  chmod +x "./$REPO_NAME/install.sh"
+  ./"$REPO_NAME/install.sh"
+fi
+
+echo "Folders copied. Cleaning up!"
+rm -drf "$CLONE_DIR"
+echo "Cloned repo deleted."
+
+echo "Script finished. Theme installation complete."
+
+
+# ---
+# echo 'Preparing directory for cloning...'
+
+# mkdir -p /tmp/clone/MoreWaita/
+# cd /tmp/clone/MoreWaita/
+# echo 'Directory created.'
+
+# git clone https://github.com/somepaulo/MoreWaita.git
+# echo 'Repo cloned. Running install script...'
+
+# MoreWaita/install.sh
+# cho 'Install script finished. Removing cloned repo...'
+
+# rm -r MoreWaita
+# echo 'Cloned repo deleted.'

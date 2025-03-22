38846fb8919b86a06066f456435749b96367e544
askpng <81590263+askpng@users.noreply.github.com>
2025-03-22 10:58:58 +0700

    chore: :wrench: migrate solarizzed-gnome to use dnf

--- files/scripts/solarizzed/citron.sh
+++ files/scripts/solarizzed/citron.sh
@@ -10,18 +10,18 @@ APPNAME=citron
 
 echo 'Downloading latest version of $APPNAME from $GITOWNER/$GITREPO'.
 
-URL="https://github.com/$GITOWNER/$GITREPO/releases/download/$(curl https://api.github.com/repos/$GITOWNER/$GITREPO/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")/Citron-$(curl https://api.github.com/repos/$GITOWNER/$GITREPO/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")-anylinux-x86_64_v3.AppImage"
+URL="https://github.com/$GITOWNER/$GITREPO/releases/download/v$(curl https://api.github.com/repos/$GITOWNER/$GITREPO/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")/Citron-v$(curl https://api.github.com/repos/$GITOWNER/$GITREPO/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")-anylinux-x86_64_v3.AppImage"
 
 echo "Downloading $URL as $APPDIR/$APPNAME.AppImage"
 
-mkdir -p $APPDIR
+# mkdir -p $APPDIR
 
-curl -L "$URL" -o "$APPDIR/$APPNAME.AppImage"
+# curl -L "$URL" -o "$APPDIR/$APPNAME.AppImage"
 
 echo "Download finished! Making $APPNAME executable..."
-chmod +x $APPDIR/$APPNAME.AppImage
+# chmod +x $APPDIR/$APPNAME.AppImage
 
 echo "Creating symlink to /usr/bin/$APPNAME..."
-ln -sf $APPDIR/$APPNAME.AppImage /usr/bin/$APPNAME
+# ln -sf $APPDIR/$APPNAME.AppImage /usr/bin/$APPNAME
 
 echo "/usr/bin/$APPNAME symlink created!"

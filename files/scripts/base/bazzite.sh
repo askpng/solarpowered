81eb8ad12965f4ddbe97ec0c1155f86552ed6f94
askpng <81590263+askpng@users.noreply.github.com>
2025-03-29 16:01:20 +0700

    chore: :hammer: update bazzite kernel install script & disable NF Code DL

--- files/scripts/base/bazzite.sh
+++ files/scripts/base/bazzite.sh
@@ -2,20 +2,21 @@
 
 set -ouex pipefail
 
-KERNEL_TAG=6.13.7-109
-KERNEL_VERSION=6.13.7-109
-OS_VERSION=41
+GIT=https://github.com/bazzite-org/kernel-bazzite
+GITOWNER=$(echo "$GIT" | sed -E 's#https://github.com/([^/]+)/([^/]+)(\.git)*#\1#')
+GITREPO=$(echo "$GIT" | sed -E 's#https://github.com/([^/]+)/([^/]+)(\.git)*#\2#')
 
-echo 'Removing Fedora kernel.'
-dnf remove -y kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra # kernel-uki-virt
+KERNEL_TAG=$(curl https://api.github.com/repos/$GITOWNER/$GITREPO/releases/latest | grep tag_name | cut -d : -f2 | tr -d "v\", ")
+KERNEL_VERSION=$KERNEL_TAG
+OS_VERSION=42
 
 echo 'Installing Bazzite kernel.'
 dnf5 install -y \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-core-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-modules-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-modules-core-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-modules-extra-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-uki-virt-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm # \
-    #c https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-devel-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
-    # https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-devel-matched-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/$GITOWNER/$GITREPO/releases/download/$KERNEL_TAG/kernel-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/$GITOWNER/$GITREPO/releases/download/$KERNEL_TAG/kernel-core-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/$GITOWNER/$GITREPO/releases/download/$KERNEL_TAG/kernel-modules-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/$GITOWNER/$GITREPO/releases/download/$KERNEL_TAG/kernel-modules-core-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/$GITOWNER/$GITREPO/releases/download/$KERNEL_TAG/kernel-modules-extra-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/$GITOWNER/$GITREPO/releases/download/$KERNEL_TAG/kernel-uki-virt-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm # \
+    #c https://github.com/$GITOWNER/$GITREPO/releases/download/$KERNEL_TAG/kernel-devel-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
+    # https://github.com/$GITOWNER/$GITREPO/releases/download/$KERNEL_TAG/kernel-devel-matched-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \

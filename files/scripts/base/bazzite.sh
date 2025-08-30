a93300b5714eb9f13bba2e92ae578651205870a1
askpng <81590263+askpng@users.noreply.github.com>
2025-08-31 02:01:51 +0700

    fix: :ambulance: temporarily disable remove zenergy

--- files/scripts/base/bazzite.sh
+++ files/scripts/base/bazzite.sh
@@ -21,26 +21,26 @@ dnf5 install -y \
     https://github.com/$GITOWNER/$GITREPO/releases/download/$KERNEL_TAG/kernel-devel-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm \
     https://github.com/$GITOWNER/$GITREPO/releases/download/$KERNEL_TAG/kernel-devel-matched-$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64.rpm
 
-echo 'Downloading ublue-os akmods COPR repo file'
-curl -L https://copr.fedorainfracloud.org/coprs/ublue-os/akmods/repo/fedora-$(rpm -E %fedora)/ublue-os-akmods-fedora-$(rpm -E %fedora).repo -o /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+# echo 'Downloading ublue-os akmods COPR repo file'
+# curl -L https://copr.fedorainfracloud.org/coprs/ublue-os/akmods/repo/fedora-$(rpm -E %fedora)/ublue-os-akmods-fedora-$(rpm -E %fedora).repo -o /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 
-echo 'Installing zenergy kmod'
-dnf5 install -y \
-    akmod-zenergy-*.fc$OS_VERSION.x86_64
-    # akmod-zenpower3-*.fc$OS_VERSION.x86_64 \
-    # akmod-ryzen-smu-*.fc$OS_VERSION.x86_64 \
+# echo 'Installing zenergy kmod'
+# dnf5 install -y \
+#     akmod-zenergy-*.fc$OS_VERSION.x86_64
+#     # akmod-zenpower3-*.fc$OS_VERSION.x86_64 \
+#     # akmod-ryzen-smu-*.fc$OS_VERSION.x86_64 \
 
-akmods --force --kernels $KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64 --kmod zenergy
-modinfo /usr/lib/modules/$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64/extra/zenergy/zenergy.ko.xz > /dev/null \
-    || (find /var/cache/akmods/zenergy/ -name \*.log -print -exec cat {} \; && exit 1)
+# akmods --force --kernels $KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64 --kmod zenergy
+# modinfo /usr/lib/modules/$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64/extra/zenergy/zenergy.ko.xz > /dev/null \
+#     || (find /var/cache/akmods/zenergy/ -name \*.log -print -exec cat {} \; && exit 1)
 
-# akmods --force --kernels $KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64 --kmod zenpower3
-# modinfo /usr/lib/modules/$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64/extra/zenpower3/zenpower3.ko.xz > /dev/null \
-#     || (find /var/cache/akmods/zenpower3/ -name \*.log -print -exec cat {} \; && exit 1)
+# # akmods --force --kernels $KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64 --kmod zenpower3
+# # modinfo /usr/lib/modules/$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64/extra/zenpower3/zenpower3.ko.xz > /dev/null \
+# #     || (find /var/cache/akmods/zenpower3/ -name \*.log -print -exec cat {} \; && exit 1)
 
-# akmods --force --kernels $KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64 --kmod ryzen-smu
-# modinfo /usr/lib/modules/$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64/extra/ryzen-smu/ryzen-smu.ko.xz > /dev/null \
-#     || (find /var/cache/akmods/ryzen-smu/ -name \*.log -print -exec cat {} \; && exit 1)
+# # akmods --force --kernels $KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64 --kmod ryzen-smu
+# # modinfo /usr/lib/modules/$KERNEL_VERSION.bazzite.fc$OS_VERSION.x86_64/extra/ryzen-smu/ryzen-smu.ko.xz > /dev/null \
+# #     || (find /var/cache/akmods/ryzen-smu/ -name \*.log -print -exec cat {} \; && exit 1)
 
-echo 'Removing ublue-os akmods COPR repo file'
-rm /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+# echo 'Removing ublue-os akmods COPR repo file'
+# rm /etc/yum.repos.d/_copr_ublue-os-akmods.repo

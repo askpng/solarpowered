3827c1390832258889b93634e48fa1b4f6e6f790
askpng <eincrad+github@gmail.com>
2025-11-15 22:49:02 +0700

    refactor: :recycle: Refactor kernel-bazzite installation script & justfile

--- .justfile
+++ .justfile
@@ -1,7 +1,37 @@
-build-tmp:
-    #!/usr/bin/env bash
-    bluebuild build --skip-validation -a ./ ./recipes/images/tmp.yml
-    rm -f ./tmp.tar.gz ./temporary.tar.gz
-    rm -rf ./.bluebuild-scripts_*
+RCPDIR := "./recipes/images"
+VERSION := "local"
 
-    
+build *ARGS:
+    #!/usr/bin/env bash
+    bluebuild build --skip-validation -s -c zstd -a "{{ RCPDIR }}"/"{{ ARGS }}".yml
+
+make-tar *ARGS:
+    #!/usr/bin/env bash
+    bluebuild build --skip-validation -s -c zstd -a ./ "{{ RCPDIR }}"/"{{ ARGS }}".yml
+    rm -f ./tmp.tar.gz
+    rm -rf ./.bluebuild-scripts_*    
+
+qcow2 *ARGS:
+    #!/usr/bin/env bash
+    set -euo pipefail
+    bluebuild generate -o Containerfile."{{ ARGS }}" "{{ RCPDIR }}"/"{{ ARGS }}".yml
+    sudo podman build -t "{{ ARGS }}":"{{ VERSION }}" --file Containerfile."{{ ARGS }}" --squash
+    sudo podman run --rm -it --privileged \
+        --security-opt label=type:unconfined_t \
+        -v /var/lib/containers/storage:/var/lib/containers/storage \
+        -v .:/output \
+        -v ./config.toml:/config.toml:ro \
+        quay.io/centos-bootc/bootc-image-builder:latest \
+        --rootfs btrfs \
+        --use-librepo=True \
+        --chown 1000:1000 \
+        localhost/"{{ ARGS }}":"{{ VERSION }}"
+    sudo rm ./manifest*
+    sudo rm -rf ./.bluebuild*
+    rm Containerfile."{{ VERSION }}"
+
+scrub:
+    #!/usr/bin/env bash
+    podman rmi -f $(podman images -f "dangling=true" -q)
+    sudo podman rmi -f $(sudo podman images -f "dangling=true" -q)
+    rm -rf qcow2
--- files/scripts/base/bazzite.sh
+++ files/scripts/base/bazzite.sh
@@ -4,35 +4,22 @@ set -ouex pipefail
 
 OS_VERSION=$(rpm -E %fedora)
 
-SEARCH_STRING=".fc${OS_VERSION}.x86_64.rpm"
-
-KERNEL_TAG=$(curl -s "https://api.github.com/repos/bazzite-org/kernel-bazzite/releases" | \
-             jq -r --arg search_str "$SEARCH_STRING" \
-             '.[] | select(.assets[] | .name | endswith($search_str)) | .tag_name' | \
-             sort -V | tail -n 1)
-
-if [[ -z "$KERNEL_TAG" ]]; then
-    echo "Error: No release with assets matching '$SEARCH_STRING'"
-    exit 1
-fi
-
-echo "Found matching kernel tag: $KERNEL_TAG"
-
+VER=$(basename $(curl -Ls -o /dev/null -w %{url_effective} https://github.com/bazzite-org/kernel-bazzite/releases/latest))
 
 dnf -y remove kernel-* && rm -drf /usr/lib/modules/*
 
 echo 'Installing Bazzite kernel...'
 dnf install -y \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-common-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-core-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-devel-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-devel-matched-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-modules-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-modules-akmods-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-modules-core-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-modules-extra-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-modules-extra-matched-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-modules-internal-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-tools-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm \
-    https://github.com/bazzite-org/kernel-bazzite/releases/download/$KERNEL_TAG/kernel-tools-libs-$KERNEL_TAG.fc$OS_VERSION.x86_64.rpm
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-common-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-core-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-devel-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-devel-matched-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-modules-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-modules-akmods-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-modules-core-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-modules-extra-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-modules-extra-matched-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-modules-internal-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-tools-$VER.fc$OS_VERSION.x86_64.rpm \
+    https://github.com/bazzite-org/kernel-bazzite/releases/download/$VER/kernel-tools-libs-$VER.fc$OS_VERSION.x86_64.rpm

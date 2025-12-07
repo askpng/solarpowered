09c4a6982e3857bfcc076197dc448ba6c685c5fc
askpng <eincrad+github@gmail.com>
2025-12-08 01:47:14 +0700

    chore: :wrench: Override vm.swappiness value from cachyos-settings when using kernel-cachy

--- files/scripts/kernels/kernel-cachy.sh
+++ files/scripts/kernels/kernel-cachy.sh
@@ -17,8 +17,7 @@ dnf -y copr enable bieszczaders/kernel-cachyos-addons
 dnf -y copr enable ublue-os/akmods
 dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-multimedia.repo
 
-
-# Handles kernel post-transcaction scriptlet
+# Handles kernel post-transaction scriptlet
 # mv /usr/lib/kernel/install.d/05-rpmostree.install /usr/lib/kernel/install.d/05-rpmostree.install.bak
 # mv /usr/lib/kernel/install.d/50-dracut.install /usr/lib/kernel/install.d/50-dracut.install.bak
 # printf '%s\n' '#!/bin/sh' 'exit 0' > /usr/lib/kernel/install.d/05-rpmostree.install
@@ -39,7 +38,7 @@ dnf -y install --setopt=install_weak_deps=False \
     scx-manager
 dnf -y swap zram-generator-defaults cachyos-settings
 
-# Handles kernel post-transcaction scriptlet
+# Handles kernel post-transaction scriptlet
 # rm -f /usr/lib/kernel/install.d/05-rpmostree.install \
 #       /usr/lib/kernel/install.d/50-dracut.install
 # mv /usr/lib/kernel/install.d/05-rpmostree.install.bak /usr/lib/kernel/install.d/05-rpmostree.install
@@ -51,6 +50,9 @@ VER=$(ls /lib/modules) && \
     akmods --force --kernels $VER --kmod evdi && \
     depmod -a $VER && \
     dracut --kver $VER --force --add ostree --no-hostonly --reproducible /usr/lib/modules/$VER/initramfs.img
+
+# Set vm.swappiness to 180, as a potential fix to audio stuttering issue upon resume
+sed -i 's/^vm\.swappiness[[:space:]]*=[[:space:]]*[0-9]*/vm.swappiness = 180/' /usr/lib/sysctl.d/99-cachyos-settings.conf
 
 # Clean up repos from earlier
 rm -f /etc/yum.repos.d/{*copr*,*multimedia*}

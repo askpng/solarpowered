86e19292ee834647d09a582cdd7b455b46ada681
askpng <eincrad+github@gmail.com>
2026-02-15 14:45:45 +0700

    test: :truck: Rename to edid.bin

--- files/scripts/shared/edid.sh
+++ files/scripts/shared/edid.sh
@@ -3,5 +3,5 @@
 set -euox pipefail
 
 tee "/etc/dracut.conf.d/00-dp-2.conf" > /dev/null << 'EOF'
-install_items+=" /lib/firmware/edid/dp-dp-2.bin "
+install_items+=" /lib/firmware/edid/edid.bin "
 EOF
